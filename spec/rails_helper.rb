e310d26aa63510f2b183847e287669ab15564839
Steve Gibbs <swaygibb@gmail.com>
2025-02-13 10:15:57 -0600

    Set up authorization token in RSpec before each test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,4 +67,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:each) do
+    request.headers['Authorization'] = ENV['AUTH_TOKEN']
+  end
 end
