fbb08d5a5b367c34c1ff848847d5dcb79fd918a3
Steve Gibbs <swaygibb@gmail.com>
2025-02-14 10:04:49 -0600

    Add Devise and JWT authentication with user model and sessions controller

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,13 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: { message: 'Signed up successfully.', user: resource }, status: :ok
+    else
+      render json: { error: resource.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_13_191126) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_14_155603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_13_191126) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "jwt_denylists", force: :cascade do |t|
+    t.string "jti"
+    t.datetime "exp"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylists_on_jti"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -50,5 +58,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_13_191126) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "inventory_quantity"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
